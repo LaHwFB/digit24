b2830147a4a6c84eee3dc8599d4e66039c15633f
LaHwFB <39505215+LaHwFB@users.noreply.github.com>
2018-08-08 17:42:52 +0100

    Add files via upload

---
+++ src/components/DefaultInput/DefaultInput.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {TextInput, StyleSheet } from 'react-native';
+
+const defaultInput = props => (
+  <TextInput
+    style={styles.input}
+    underlineColorAndroid='transparent'
+    {...props}
+    style={ [styles.input, props.style] } />
+
+);
+
+const styles=StyleSheet.create({
+  input: {
+    width: '100%',
+    borderWidth: 1,
+    borderColor: 'grey',
+    padding: 5,
+    margin: 8
+  }
+});
+
+export default defaultInput;
--- src/screens/Auth/Auth.js
+++ src/screens/Auth/Auth.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, TextInput, StyleSheet } from 'react-native';
 
 import startMainTabs from '../MainTabs/startMainTabs';
+import DefaultInput from '../../components/DefaultInput/DefaultInput';
 
 class AuthScreen extends Component {
     loginHandler = () => {
@@ -10,12 +11,34 @@ class AuthScreen extends Component {
 
     render () {
         return (
-            <View>
-                <Text>Auth Screen</Text>
-                <Button title="Login" onPress={this.loginHandler}/>
+            <View style={styles.container}>
+                <Text>Please Log In</Text>
+                <Button title='switch to Login' />
+                <View style={styles.inputContainer}>
+                <DefaultInput placeholder='Email adrress' style={[styles.input ,{borderColor:'brown'}] }/>
+                <DefaultInput placeholder='Password' style={styles.input}/>
+                <DefaultInput placeholder='confirm Password' style={styles.input}/>
+                </View>
+                <Button title="submit" onPress={this.loginHandler}/>
             </View>
         );
     }
 }
+
+const styles=StyleSheet.create({
+  container: {
+      borderWidth: 2,
+      flex:1,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  inputContainer: {
+    width: '80%'
+  },
+  input:{
+    backgroundColor: 'white',
+    borderColor: 'black'
+  }
+});
 
 export default AuthScreen;
