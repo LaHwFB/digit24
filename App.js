6b413e1b68719b5d48c67a8eddcd76c559213c83
LaHwFB <39505215+LaHwFB@users.noreply.github.com>
2018-08-14 01:20:52 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -26,6 +26,36 @@ export default class App extends Component {
     });
   };
 
+  fetch('http://localhost/phpmyadmin/sql.php?db=test&table=users&server=1&target=&token=6866efe72c2d999986e8f2070ee2cea0#PMAURL-9:sql.php?db=covoiturage&table=cars&server=1&target=&token=20e74a0884c548639265bd5daefe8b2f', {
+  method: 'POST',
+  headers: {
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    key : id,
+
+    name: name,
+
+    image: image,
+
+    portes: portes
+
+  })
+
+}).then((response) => response.json())
+      .then((responseJson) => {
+
+// Showing response message coming from server after inserting records.
+        Alert.alert(responseJson);
+
+      }).catch((error) => {
+        console.error(error);
+      });
+
+
+  }
+
   placeDeletedHandler = () => {
     this.setState(prevState => {
       return {
@@ -55,18 +85,56 @@ export default class App extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <PlaceDetail
-          selectedPlace={this.state.selectedCar}
-          onItemDeleted={this.placeDeletedHandler}
-          onModalClosed={this.modalClosedHandler}
+      <View >
+
+        <TextInput
+
+          // Adding hint in Text Input using Place holder.
+          placeholder="Enter Name"
+
+          onChangeText={TextInputName => this.setState({TextInputName})}
+
+          // Making the Under line Transparent.
+          underlineColorAndroid='transparent'
+
+          style={styles.TextInputStyleClass}
         />
-        <PlaceInput onPlaceAdded={this.placeAddedHandler} />
-        <PlaceList
-          places={this.state.places}
-          onItemSelected={this.placeSelectedHandler}
+
+        <TextInput
+
+          // Adding hint in Text Input using Place holder.
+          placeholder="Enter image"
+
+          onChangeText={TextInputEmail => this.setState({TextInputEmail})}
+
+          // Making the Under line Transparent.
+          underlineColorAndroid='transparent'
+
+          style={styles.TextInputStyleClass}
         />
-      </View>
+
+        <TextInput
+
+          // Adding hint in Text Input using Place holder.
+          placeholder="Enter Portes"
+
+          onChangeText={TextInputPhoneNumber => this.setState({TextInputPhoneNumber})}
+
+          // Making the Under line Transparent.
+          underlineColorAndroid='transparent'
+
+          style={styles.TextInputStyleClass}
+        />
+
+        <Button title="Insert Text Input Data to Server" onPress={this.InsertDataToServer} color="#2196F3" />
+
+
+
+</View>
+
+    );
+  }
+}
     );
   }
 }
